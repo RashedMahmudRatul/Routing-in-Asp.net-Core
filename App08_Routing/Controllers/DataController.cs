18e7aa06c93c145089482e7fa2af7056a5e1ab82
Rashed Mahmud <rashedmahmud68@gmail.com>
2020-10-12 07:56:25 +0600

    Add project files.

---
+++ App08_Routing/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App08_Routing.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+        public IActionResult Hide()
+        {
+            return View();
+        }
+    }
+}
